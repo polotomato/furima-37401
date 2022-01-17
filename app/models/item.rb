d5eff71396c067f37f69da9cead2ce7925faff06
polotomato <aotomato@gmail.com>
2022-01-18 00:48:43 +0900

    Item モデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,22 @@ class Item < ApplicationRecord
   # テーブル関連
   has_one_attached :image
   belongs_to :user
+
+  # バリデーション
+  validates :image, presence: { message: "must be attaced" }
+
+  with_options presence: true do
+    validates :name,        length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_fee_status_id
+      validates :prefecture_id
+      validates :scheduled_delivery_id
+    end
+    validates :price, numericality: { greater_than_or_equal_to: 300,
+                                         less_than_or_equal_to: 9_999_999 } 
+  end
 end
