cc0b2dd8eff38fae04cb3b3f8a45cefb3b89cdef
polotomato <aotomato@gmail.com>
2022-01-17 15:40:43 +0900

    Active Hash関連のアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :prefecture,
+             :scheduled_delivery, :shipping_fee_status
 end
