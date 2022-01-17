36ec3d06ccf73fc38a9130f55a3070d85b2290c5
polotomato <aotomato@gmail.com>
2022-01-17 15:44:56 +0900

    item と user のアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
+  # ActiveHash 関連
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :prefecture,
              :scheduled_delivery, :shipping_fee_status
+  
+  # テーブル関連
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,6 @@ class User < ApplicationRecord
       validates :first_name_kana
     end
   end
+
+  has_many :items
 end
