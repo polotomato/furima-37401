bed5790cfca0517bed4caa6d85afcadcc2405d61
polotomato <aotomato@gmail.com>
2022-01-18 14:38:50 +0900

    Item モデルの関連付けの書き方を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
   # ActiveHash 関連
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :prefecture,
-             :scheduled_delivery, :shipping_fee_status
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+  belongs_to :shipping_fee_status
   
   # テーブル関連
   has_one_attached :image
