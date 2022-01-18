c35e5f970206135187072d64515e97670af8c3ed
polotomato <aotomato@gmail.com>
2022-01-18 14:41:11 +0900

    価格は整数のみのバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -26,6 +26,7 @@ class Item < ApplicationRecord
       validates :scheduled_delivery_id
     end
     validates :price, numericality: {
+      only_integer: true,
       greater_than_or_equal_to: 300,
       less_than_or_equal_to: 9_999_999
     }
