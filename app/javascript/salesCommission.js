0ecdc1bbd0cf04dcbb14b9f69af158edadfc110e
polotomato <aotomato@gmail.com>
2022-01-18 14:34:54 +0900

    定数をconstに書き換え

--- app/javascript/salesCommission.js
+++ app/javascript/salesCommission.js
@@ -1,13 +1,13 @@
 function catchPrice (){
-  var price = document.getElementById("item-price");
-  var tax = document.getElementById("add-tax-price");
-  var profit = document.getElementById("profit");
+  const price = document.getElementById("item-price");
+  const tax = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
 
   if (null == price) return;
 
   price.addEventListener('input', () => {
-    var p = price.value;
-    var t = Math.floor( p * 0.1 );
+    const p = price.value;
+    const t = Math.floor( p * 0.1 );
 
     tax.textContent = t.toLocaleString();
     profit.textContent = (p - t).toLocaleString();
