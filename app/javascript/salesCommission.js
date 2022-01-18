50a77a81cf988698d31eedb2e333e21724125000
polotomato <aotomato@gmail.com>
2022-01-18 23:02:13 +0900

    salesCommission.js を修正
    ・値を更新する部分を関数化
    ・編集画面に入った時に実行するように修正

--- app/javascript/salesCommission.js
+++ app/javascript/salesCommission.js
@@ -1,17 +1,24 @@
-function catchPrice (){
-  const price = document.getElementById("item-price");
+function updateValue() {
+  const bb = document.getElementById("item-price");
   const tax = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
+  
+  const p = bb.value;
+  const t = Math.floor( p * 0.1 );
+
+  tax.textContent = t.toLocaleString();
+  profit.textContent = (p - t).toLocaleString();
+};
+
+function catchPrice (){
+  const price = document.getElementById("item-price");
 
   if (null == price) return;
 
-  price.addEventListener('input', () => {
-    const p = price.value;
-    const t = Math.floor( p * 0.1 );
+  // 編集画面用 初回ロード時、利益・手数料を表示
+  updateValue();
 
-    tax.textContent = t.toLocaleString();
-    profit.textContent = (p - t).toLocaleString();
-  });
+  price.addEventListener('input', updateValue);
 };
 
 window.addEventListener('load', catchPrice);
