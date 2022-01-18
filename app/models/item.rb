6a371fed97fc7d83ec5c26bfae6f03df5ae7045f
polotomato <aotomato@gmail.com>
2022-01-18 13:45:09 +0900

    Rubocop 整形

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,7 +22,9 @@ class Item < ApplicationRecord
       validates :prefecture_id
       validates :scheduled_delivery_id
     end
-    validates :price, numericality: { greater_than_or_equal_to: 300,
-                                         less_than_or_equal_to: 9_999_999 } 
+    validates :price, numericality: {
+      greater_than_or_equal_to: 300,
+      less_than_or_equal_to: 9_999_999
+    }
   end
 end
