6e6fa960e7f60b5b3acc8a8151778c84d99ca4a1
polotomato <aotomato@gmail.com>
2022-01-19 23:35:25 +0900

    購入画面用のインスタンスを設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,8 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
-  
+
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
